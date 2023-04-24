6dd15e433f99ee9fd87b1b110ed56309ed791fef
Koen Verheyen <verheyen.koen@gmail.com>
2023-04-24 15:15:30 +0200

    Extended projects page test (with visits to 3 random navigation pages and detail page)

--- cypress/e2e/projects.spec.ts
+++ cypress/e2e/projects.spec.ts
@@ -1,5 +1,15 @@
+import { getAlphabet } from 'cypress/support/util'
+
 describe('The Projects page', () => {
-  it('should load without errors', () => {
-    cy.visit('/project')
+  const pages = Cypress._.pull(getAlphabet(), 'X').takeRandomSet(3)
+
+  pages.forEach(page => {
+    it(`should be possible to use pagination and the visit detail pages (${page})`, () => {
+      cy.visit('/project')
+
+      cy.contains('.pagination > li', page).click()
+
+      cy.get('.result-item a[href*="/project/"]').random().click()
+    })
   })
 })
