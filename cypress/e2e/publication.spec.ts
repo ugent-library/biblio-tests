72940c2527b350a34986411fcaceb23b636d8b4b
Koen Verheyen <verheyen.koen@gmail.com>
2023-04-25 09:39:42 +0200

    Added failing test for issue https://github.ugent.be/Universiteitsbibliotheek/biblio/issues/77 (currently skipped)

--- cypress/e2e/publication.spec.ts
+++ cypress/e2e/publication.spec.ts
@@ -2,4 +2,12 @@ describe('The publication page', () => {
   it('should load without errors', () => {
     cy.visit('/publication/8565192')
   })
+
+  it.skip('should call the times-cited API', () => {
+    cy.intercept('/info/times-cited*').as('timesCited')
+
+    cy.visit('/publication/8731393')
+
+    cy.get('@timesCited').should('have.nested.property', 'response.statusCode', 200)
+  })
 })
